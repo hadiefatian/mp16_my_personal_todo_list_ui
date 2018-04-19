4d86b98dd796812491e17c6a73f067cbd6e929b9
Hadi <hadi.effatian@gmail.com>
2018-04-19 16:21:41 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp16_my_personal_todo_list_ui/Mp16MyPersonalTodoListUiApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp16_my_personal_todo_list_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp16MyPersonalTodoListUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp16MyPersonalTodoListUiApplication.class, args);
+	}
+}
